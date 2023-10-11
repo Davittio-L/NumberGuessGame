a5955581bd01897525f4163499fe60a97f4efcd5
Colten Bayles <cbayles@outlook.com>
2023-10-11 19:21:44 -0400

    added if statement

--- guessing.py
+++ guessing.py
@@ -7,3 +7,8 @@ random_number = random.randint(1, 11)
 for random_number in range(1,  7):
     print('Take a guess...')
     guess = int(input())
+
+    if guess < random_number:
+        print("Your guess was to low.")
+    elif guess > random_number:
+        print("Your guess was to high.")

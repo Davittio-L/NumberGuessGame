47be1515c0ff9c362cbc7395123ea382660d02f9
Colten Bayles <cbayles@outlook.com>
2023-10-12 19:12:25 -0400

    Adding difficulties to the guessing game.

--- guessing.py
+++ guessing.py
@@ -1,21 +1,25 @@
 import random
 
-print("I am thinking of a number between 1 & 10")
+choice = input("Please choose a difficulty: Easy, Medium, Hard: ")
 
-random_number = random.randint(1, 11)
+if choice == lowercase("easy"):
 
-for guessTaken in range(1,  7):
-    print('Take a guess...')
-    guess = int(input())
+    print("I am thinking of a number between 1 & 10")
 
-    if guess < random_number:
-        print("Your guess was to low.")
-    elif guess > random_number:
-        print("Your guess was to high.")
+    random_number = random.randint(1, 11)
+
+    for guessTaken in range(1,  7):
+        print('Take a guess...')
+        guess = int(input())
+
+        if guess < random_number:
+            print("Your guess was to low.")
+        elif guess > random_number:
+            print("Your guess was to high.")
+        else:
+            break
+
+    if guess == random_number:
+        print('Good job you guessed the correct number in ' + str(guessTaken) + 'guesses!')
     else:
-        break
-
-if guess == random_number:
-    print('Good job you guessed the correct number in ' + str(guessTaken) + 'guesses!')
-else:
-    print('You ran out of guesses! The number was ' + str(random_number))
+        print('You ran out of guesses! The number was ' + str(random_number))
